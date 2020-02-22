9c92aea82e7edb58a79513aecedd62b2dd014d20
James <jmkgallier@gmail.com>
2020-02-21 23:52:39 -0500

    Listed future static methods

--- src/setup.py
+++ src/setup.py
@@ -1,25 +1,23 @@
 import os
-from SQLite_Manage import SQLManage
+from SQLite_CRUD import CRUD
 
 
 # Create Build Directory
 def check_build():
     build_dir = os.getcwd().replace("src", "build")
     if os.path.exists(build_dir):
-        print("Build Directory Available")
+        print("'build' Directory Available")
         return build_dir
     else:
-        print("Build Directory was not detected.")
-        print("Build Directory created.")
+        print("No 'build' Directory\nCreating 'build' Directory")
         os.mkdir(build_dir)
         return build_dir
 
 
 def check_sql(db_filename):
-    SQLManage.create_connection(db_filename)
+    CRUD.initiate_database(db_filename)
 
 
 if __name__ == '__main__':
     sql_dest = check_build()
     check_sql(os.path.join(sql_dest, "bank_SQL"))
-
